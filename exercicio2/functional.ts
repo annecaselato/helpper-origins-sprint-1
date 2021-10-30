a1a4fc1d16a42f2565d8b099b35b951598eb9324
annecaselato <88508714+annecaselato@users.noreply.github.com>
2021-10-29 22:59:27 -0300

    Update Exercicio 2

--- exercicio2/functional.ts
+++ exercicio2/functional.ts
@@ -2,7 +2,8 @@ let list: Array<any> = [
     {"id" : 1, "name": "Ada Lovelace", "bio" : "Ada Lovelace, foi uma matemática e escritora inglesa reconhecida por ter escrito o primeiro algoritmo para ser processado por uma máquina"},
     {"id" : 2, "name": "Alan Turing", "bio" : "Alan Turing foi um matemático, cientista da computação, lógico, criptoanalista, filósofo e biólogo teórico britânico, ele é amplamente considerado o pai da ciência da computação teórica e da inteligência artificia"},
     {"id" : 3, "name": "Nikola Tesla", "bio" : "Nikola Tesla foi um inventor, engenheiro eletrotécnico e engenheiro mecânico sérvio, mais conhecido por suas contribuições ao projeto do moderno sistema de fornecimento de eletricidade em corrente alternada."},
-    {"id" : 4, "name": "Nicolau Copérnico", "bio": "Nicolau Copérnico foi um astrônomo e matemático polonês que desenvolveu a teoria heliocêntrica do Sistema Solar."}];
+    {"id" : 4, "name": "Nicolau Copérnico", "bio": "Nicolau Copérnico foi um astrônomo e matemático polonês que desenvolveu a teoria heliocêntrica do Sistema Solar."}
+];
 
 //Função que retorna a bio do id passado
 function getBioFnct(givenId: number) {
@@ -18,15 +19,20 @@ function getNameFnct(givenId: number) {
 
 //Função que apaga o item do id passado
 function deleteItemFnct(givenId: number) {
+    if (givenId >= list.length) {
+        return;
+    }
     let newList: Array<any> = list.filter(obj => obj.id !==givenId);
     return newList;
 }
 
 //Função que permite alterar o name ou a bio do id passado
 function updateItemFnct(givenId: number, newName: string, newBio: string) {
-    let objIndex: number = list.findIndex((obj => obj.id == 1));
+    if (givenId >= list.length) {
+        return;
+    }
     let newList: Array<any> = list;
-    newList[objIndex].name = newName;
-    newList[objIndex].bio = newBio;
+    newList[givenId - 1].name = newName;
+    newList[givenId - 1].bio = newBio;
     return newList;
 }
--- exercicio2/imperative.ts
+++ exercicio2/imperative.ts
@@ -2,11 +2,12 @@ let impList: Array<any> = [
     {"id" : 1, "name": "Ada Lovelace", "bio" : "Ada Lovelace, foi uma matemática e escritora inglesa reconhecida por ter escrito o primeiro algoritmo para ser processado por uma máquina"},
     {"id" : 2, "name": "Alan Turing", "bio" : "Alan Turing foi um matemático, cientista da computação, lógico, criptoanalista, filósofo e biólogo teórico britânico, ele é amplamente considerado o pai da ciência da computação teórica e da inteligência artificia"},
     {"id" : 3, "name": "Nikola Tesla", "bio" : "Nikola Tesla foi um inventor, engenheiro eletrotécnico e engenheiro mecânico sérvio, mais conhecido por suas contribuições ao projeto do moderno sistema de fornecimento de eletricidade em corrente alternada."},
-    {"id" : 4, "name": "Nicolau Copérnico", "bio": "Nicolau Copérnico foi um astrônomo e matemático polonês que desenvolveu a teoria heliocêntrica do Sistema Solar."}];
+    {"id" : 4, "name": "Nicolau Copérnico", "bio": "Nicolau Copérnico foi um astrônomo e matemático polonês que desenvolveu a teoria heliocêntrica do Sistema Solar."}
+];
 
 //Função que retorna a bio do id passado
 function getBioImp(givenId: number) {
-    let bio = undefined;
+    let bio: any = undefined;
     for (let i = 0; i < impList.length; i++) {
         if (impList[i].id === givenId) {
             bio = impList[i].bio;
@@ -17,7 +18,7 @@ function getBioImp(givenId: number) {
 
 //Função que retorna o name do id passado
 function getNameImp(givenId: number) {
-    let name = undefined;
+    let name:any = undefined;
     for (let i = 0; i < impList.length; i++) {
         if (impList[i].id === givenId) {
             name = impList[i].name;
@@ -28,10 +29,13 @@ function getNameImp(givenId: number) {
 
 //Função que apaga o item do id passado
 function deleteItemImp(givenId: number) {
-    let newList = impList;
+    if (givenId >= impList.length) {
+        return;
+    }
+    let newList: Array<any> = impList;
     for (let i = 0; i < impList.length; i++) {
         if (impList[i].id === givenId) {
-            newList;
+            newList.splice(i, 1);
         }
     }
     return newList;
@@ -39,7 +43,10 @@ function deleteItemImp(givenId: number) {
 
 //Função que permite alterar o name ou a bio do id passado
 function updateItemImp(givenId: number, newName: string, newBio: string) {
-    let newList = impList;
+    if (givenId >= impList.length) {
+        return;
+    }
+    let newList: Array<any> = impList;
     for (let i = 0; i < impList.length; i++) {
         if (impList[i].id === givenId) {
             newList[i].name = newName;
