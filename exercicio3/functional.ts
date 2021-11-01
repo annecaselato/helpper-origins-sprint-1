d2f6d6f12f5d7f0546d3cf58758443c81997f441
annecaselato <88508714+annecaselato@users.noreply.github.com>
2021-10-31 21:24:00 -0300

    Update exercicio3

--- exercicio3/functional.ts
+++ exercicio3/functional.ts
@@ -1,21 +1,13 @@
-//Função que retorna o maior valor, o menor valor e o valor médio de uma lista recebida, utilizando o paradigma funcional
-function fnctFunction (list: any[]) {
-    //Valida se a lista é numérica
-    function isNumberArray (list: any[]) {
-        return Array.isArray(list) && list.every((value) => typeof value === 'number')
-    }
-    if(!isNumberArray(list)) {
-        return;
-    }
-    
+//Função que retorna o maior valor, o menor valor e o valor médio de uma lista numérica recebida, utilizando o paradigma funcional
+function inspectListByFunctional(inputList: number[]): number[] { 
     //Ordena a lista para encontrar os valores maior e menor
-    let sortedList: number[] = list.sort((n1, n2) => n1 - n2);
-    let minValue = sortedList [0];
-    let maxValue = sortedList [list.length - 1];
+    let sortedList: number[] = inputList.sort((n1, n2) => n1 - n2);
+    let minValue: number = sortedList [0];
+    let maxValue: number = sortedList [inputList.length - 1];
     //Soma todos os valores e calcula o valor médio
-    let sum = list.reduce((a, b) => a + b, 0);
-    let avgValue = sum/list.length;
+    let sum: number = inputList.reduce((a, b) => a + b, 0);
+    let avgValue: number = sum/inputList.length;
     
-    let answerList = [maxValue, minValue, avgValue];
-    return answerList;
+    let outputList: number[] = [maxValue, minValue, avgValue];
+    return outputList;
     }
--- exercicio3/imperative.ts
+++ exercicio3/imperative.ts
@@ -1,30 +1,23 @@
-//Função que retorna o maior valor, o menor valor e o valor médio de uma lista recebida, utilizando o paradigma imperativo
-function impFunction (inputList: any[]) {
-    //Valida se a lista é numérica
-    for (let j = 0; j < inputList.length; j++) {
-        if (typeof(inputList[j]) !== 'number') {
-            return;
-        }
-    }
-
-    let maxValue = inputList[0];
-    let minValue = inputList[0];
-    let sum = 0;    
+//Função que retorna o maior valor, o menor valor e o valor médio de uma lista numérica, utilizando o paradigma imperativo
+function inspectListByImperative(inputList: number[]): number[] {
+    let maxValue: number = inputList[0];
+    let minValue: number = inputList[0];
+    let sum: number = 0;
     
     //Encontra o valor maior, valor menor e calcula a soma de todos os valores
-    for (let i = 0; i < inputList.length; i++) {
-        sum += inputList[i];
-        if (inputList[i] < minValue) {
-            minValue = inputList[i];
+    for (let index = 0; index < inputList.length; index++) {
+        sum += inputList[index];
+        if (inputList[index] < minValue) {
+            minValue = inputList[index];
         }
-        if (inputList[i] > maxValue) {
-            maxValue = inputList[i];
+        if (inputList[index] > maxValue) {
+            maxValue = inputList[index];
         }  
     }
     
     //Calcula o valor médio
-    let avgValue = parseFloat((sum/inputList.length).toFixed(2));
+    let avgValue: number = parseFloat((sum/inputList.length).toFixed(2));
     
-    let answerList = [maxValue, minValue, avgValue];
-    return answerList;
+    let outputList: number[] = [maxValue, minValue, avgValue];
+    return outputList;
 }
